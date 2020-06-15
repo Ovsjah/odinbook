0be79105ab47890e28720d3aec1fa55455ba56ec
Ovsjah <ovsjah@gmail.com>
2020-06-15 17:52:19 +0300

    Change config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -120,12 +120,12 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { host: host }
 
   config.action_mailer.smtp_settings = {
-      address: 'smtp.sendgrid.net',
-      port: 587,
-      authentication: :plain,
-      user_name: ENV['SENDGRID_USERNAME'],
-      password: ENV['SENDGRID_PASSWORD'],
-      domain: 'heroku.com',
-      enable_starttls_auto: true
+    user_name: ENV['SENDGRID_USERNAME'],
+    password: ENV['SENDGRID_PASSWORD'],
+    domain: 'heroku.com',
+    address: 'smtp.sendgrid.net',
+    port: 465,
+    authentication: :plain,
+    enable_starttls_auto: true
   }
 end
