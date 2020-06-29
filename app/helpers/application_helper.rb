964fe8076899cafabfef9bba93761f2c7aa6d23c
Ovsjah <ovsjah@gmail.com>
2020-06-29 18:58:46 +0300

    Add Cloudinary

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,9 +5,9 @@ module ApplicationHelper
 
   def avatar_for(user, size, class_names = 'img-fluid rounded-circle')
     if user.avatar.attached?
-      image_tag user.avatar.variant(resize_to_fill: [size, nil]), size: size, class: class_names
+      image_tag user.avatar.variant(resize_to_fill: [size, nil]), size: "#{size}x#{size}", class: class_names
     else
-      image_tag 'avatar-placeholder.gif', size: size, class: class_names
+      image_tag 'avatar-placeholder.gif', size: "#{size}x#{size}", class: class_names
     end
   end
 end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -28,8 +28,8 @@ Rails.application.configure do
     config.cache_store = :null_store
   end
 
-  # Store uploaded files on the local file system (see config/storage.yml for options).
-  config.active_storage.service = :local
+  # Use Cloudinary.
+  config.active_storage.service = :cloudinary
 
   # Persistent queue
   config.active_job.queue_adapter = :sidekiq
